3741c890759cf6c32cae6857b7a1a080234365d6
Tito Bouzout <tito.bouzout@gmail.com>
2012-02-27 10:06:42 -0300

    Add preferences

--- FunctionNameStatus.py
+++ FunctionNameStatus.py
@@ -3,20 +3,29 @@ import sublime, sublime_plugin, time
 
 # Ideas taken from C0D312, nizur & tito in http://www.sublimetext.com/forum/viewtopic.php?f=2&t=4589
 
-class FunctionNameStatusEventHandler(sublime_plugin.EventListener):
+s = sublime.load_settings('Function Name Display.sublime-settings')
 
-  def __init__(self):
-    self.wait_time = 0.02
-    self.time = time.time()
+class Pref:
+  def load(self):
+    Pref.display_class = s.get('display_class', True)
+    Pref.display_function = s.get('display_function', True)
+    Pref.wait_time = 0.02
+    Pref.time = time.time()
+
+Pref().load()
+s.add_on_change('display_class', lambda:Pref().load())
+s.add_on_change('display_function', lambda:Pref().load())
+
+class FunctionNameStatusEventHandler(sublime_plugin.EventListener):
 
   # Event handlers
   def on_selection_modified(self, view):
     now = time.time()
-    if now - self.time > self.wait_time:
-      self.time = now
+    if now - Pref.time > Pref.wait_time:
+      Pref.time = now
       self.display_current_class_and_function(view)
     else:
-      self.time = now
+      Pref.time = now
 
   def on_deactivated(self, view):
     view.erase_status('function')
@@ -26,7 +35,7 @@ class FunctionNameStatusEventHandler(sublime_plugin.EventListener):
 
   def on_activated(self, view):
      self.display_current_class_and_function(view)
-      
+
   # display the current class and function name
   def display_current_class_and_function(self, view):
     if view.settings().get('is_widget'):
@@ -35,26 +44,33 @@ class FunctionNameStatusEventHandler(sublime_plugin.EventListener):
     region = view.sel()[0]
     region_row, region_col = view.rowcol(region.begin())
 
-    # Look for any classes
     s = ""
-    class_regions = view.find_by_selector('entity.name.type.class')
-    for r in reversed(class_regions):
-      row, col = view.rowcol(r.begin())
-      if row <= region_row:
-        s = view.substr(r) + "::"
+    found = False
+
+    # Look for any classes
+    if Pref.display_class:
+      class_regions = view.find_by_selector('entity.name.type.class')
+      for r in reversed(class_regions):
+        row, col = view.rowcol(r.begin())
+        if row <= region_row:
+          s = view.substr(r)
+          found = True
+          if Pref.display_function:
+            s += "::"
+          break;
 
     # Look for any functions including PHP magic functions
-    function_regions = view.find_by_selector('entity.name.function') + view.find_by_selector('support.function.magic.php')
-    if not function_regions:
+    if Pref.display_function:
+      function_regions = view.find_by_selector('entity.name.function') + view.find_by_selector('support.function.magic.php')
+      if function_regions:
+        for r in reversed(function_regions):
+          row, col = view.rowcol(r.begin())
+          if row <= region_row:
+            s = s + view.substr(r)
+            found = True
+            break
+
+    if not found:
       view.erase_status('function')
-      return
-
-    function_regions.sort()
-    for r in reversed(function_regions):
-      row, col = view.rowcol(r.begin())
-      if row <= region_row:
-        s = s + view.substr(r)
-        view.set_status('function', s)
-        return
-
-    view.erase_status('function')
+    else:
+      view.set_status('function', s)
