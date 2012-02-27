417ea1f6c9a935bb515785db889853daef78758c
Rob Allen <rob@akrabat.com>
2012-02-27 10:17:15 +0000

    Update on_selection_modified to use time() check rather than random number and a time out. Idea from titoBouzout

--- FunctionNameStatus.py
+++ FunctionNameStatus.py
@@ -1,21 +1,22 @@
 
-import sublime, sublime_plugin
-from random import randrange
+import sublime, sublime_plugin, time
 
 # Ideas taken from C0D312, nizur & tito in http://www.sublimetext.com/forum/viewtopic.php?f=2&t=4589
-# Also, waiting concept is from facelessuser in http://www.sublimetext.com/forum/viewtopic.php?f=6&t=4960
 
 class FunctionNameStatusEventHandler(sublime_plugin.EventListener):
 
   def __init__(self):
-    self.wait_time = 100
-    self.wait_id = None
+    self.wait_time = 0.02
+    self.time = time.time()
 
   # Event handlers
   def on_selection_modified(self, view):
-    self.view = view
-    self.wait_ms = self.wait_time
-    self.wait()
+    now = time.time()
+    if now - self.time > self.wait_time:
+      self.time = now
+      self.display_current_class_and_function(view)
+    else:
+      self.time = now
 
   def on_deactivated(self, view):
     view.erase_status('function')
@@ -57,28 +58,3 @@ class FunctionNameStatusEventHandler(sublime_plugin.EventListener):
         return
 
     view.erase_status('function')
-
-
-  # wait methods so that we don't make ST2 crawl
-  def check_wait(self, wait_id):
-    if self.wait_id != wait_id:
-      new_wait_id = randrange(1, 999999)
-      self.wait_id = new_wait_id
-      sublime.set_timeout(
-        lambda: self.check_wait(wait_id=new_wait_id),
-        self.wait_ms
-      )
-    else:
-      self.wait_id = None
-      self.display_current_class_and_function(self.view)
-
-  def wait(self):
-      new_wait_id = randrange(1, 999999)
-      if self.wait_id == None:
-          self.wait_id = new_wait_id
-          sublime.set_timeout(
-              lambda: self.check_wait(wait_id=new_wait_id),
-              self.wait_ms
-          )
-      else:
-          self.wait_id = new_wait_id
