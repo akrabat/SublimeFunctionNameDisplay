08529f202ac2965781deab930cc78e6c566131cf
Tito Bouzout <tito.bouzout@gmail.com>
2012-03-13 17:41:35 -0300

    Option for displaying arguments, better catch of special functions

--- FunctionNameStatus.py
+++ FunctionNameStatus.py
@@ -1,5 +1,5 @@
 
-import sublime, sublime_plugin, thread
+import sublime, sublime_plugin, thread, re
 from time import time, sleep
 
 # Ideas taken from C0D312, nizur & tito in http://www.sublimetext.com/forum/viewtopic.php?f=2&t=4589
@@ -8,15 +8,20 @@ s = sublime.load_settings('Function Name Display.sublime-settings')
 
 class Pref:
   def load(self):
-    Pref.display_class    = s.get('display_class', True)
-    Pref.display_function = s.get('display_function', True)
-    Pref.wait_time        = 0.12
-    Pref.time             = time()
-    Pref.modified         = False
+    Pref.display_class     = s.get('display_class', False)
+    Pref.display_function  = s.get('display_function', True)
+    Pref.display_arguments = s.get('display_arguments', False)
+    Pref.wait_time         = 0.12
+    Pref.time              = time()
+    Pref.modified          = False
 
 Pref().load()
 s.add_on_change('display_class', lambda:Pref().load())
 s.add_on_change('display_function', lambda:Pref().load())
+s.add_on_change('display_arguments', lambda:Pref().load())
+
+clean_name = re.compile('^\s*(public\s+|private\s+|protected\s+|static\s+|function\s+|def\s+)+', re.I)
+
 
 class FunctionNameStatusEventHandler(sublime_plugin.EventListener):
 
@@ -68,19 +73,22 @@ class FunctionNameStatusEventHandler(sublime_plugin.EventListener):
           if row <= region_row:
             s = view.substr(r)
             found = True
-            if Pref.display_function:
-              s += "::"
             break;
 
-      # Look for any functions including PHP magic functions
+      # Look for any functions
       if Pref.display_function:
-        function_regions = view.find_by_selector('entity.name.function') + view.find_by_selector('support.function.magic.php')
+        function_regions = view.find_by_selector('meta.function')
         if function_regions:
-          function_regions.sort()
           for r in reversed(function_regions):
             row, col = view.rowcol(r.begin())
             if row <= region_row:
-              s = s + view.substr(r)
+              if Pref.display_class and s:
+                s += "::"
+              name = clean_name.sub('', view.substr(r))
+              if Pref.display_arguments:
+                s += name.strip()
+              else:
+                s += name.split('(')[0].split(':')[0].strip()
               found = True
               break
 
